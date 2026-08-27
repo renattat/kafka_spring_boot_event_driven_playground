eb2e0f83328085605e7cb8af7b99b1d35ed9ce74
Renat Sim <renat.tat@mail.ru>
2026-08-27 09:33:05 +0300

    Run producer and consumer at the same time

---
+++ src/main/java/com/renat/event_driven_playground/sec03/producer/ProducerConfig.java
@@ -0,0 +1,29 @@
+package com.renat.event_driven_playground.sec03.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@Configuration
+public class ProducerConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerConfig.class);
+
+
+    @Bean
+    public Supplier<String> producer() {
+        var counter = new AtomicInteger(0);
+        return () -> {
+            var msg = "msg-" + counter.incrementAndGet();
+            log.info("produced: {}", msg);
+            return msg;
+        };
+    }
+}
