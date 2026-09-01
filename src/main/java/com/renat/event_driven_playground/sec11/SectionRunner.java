cf4b13c52505382abf6c12bde3c75ff06792b74c
Renat Sim <renat.tat@mail.ru>
2026-09-01 13:58:36 +0300

    3 nodes kafka cluset run

---
+++ src/main/java/com/renat/event_driven_playground/sec11/SectionRunner.java
@@ -0,0 +1,31 @@
+package com.renat.event_driven_playground.sec11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class SectionRunner {
+
+    @SpringBootApplication(scanBasePackages = "com.renat.event_driven_playground.${section}.consumer")
+    static class Consumer {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Consumer.class,
+                    "--section=sec11",
+                    "--config=01-consumer");
+        }
+    }
+
+    @SpringBootApplication(scanBasePackages = "com.renat.event_driven_playground.${section}.producer")
+    static class Producer {
+
+        public static void main(String[] args) {
+            SpringApplication.run(
+                    Producer.class,
+                    "--section=sec11",
+                    "--config=02-producer");
+        }
+    }
+
+
+}
